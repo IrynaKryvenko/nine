ac113c074d76651d34f252ff5ed5e726ed9fb301
IrynaKryvenko <119738220+IrynaKryvenko@users.noreply.github.com>
2024-05-01 17:39:51 +0000

    Deploying to gh-pages from @ IrynaKryvenko/nine@2c1a0cfb269f81c29cad561fd8086a1d855402d7 🚀

--- 01-color-switcher.6569096d.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),a=document.querySelector("body"),n=null;t.addEventListener("click",(function(){n=setInterval((function(){a.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,"0"))}),1e3),e.disabled=!1,t.disabled=!0})),e.addEventListener("click",(function(){clearInterval(n),e.disabled=!0,t.disabled=!1}))}();
-//# sourceMappingURL=01-color-switcher.6569096d.js.map
