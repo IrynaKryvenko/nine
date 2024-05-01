b14e99d9ef9816d41120a099eaa68923a480aff3
IrynaKryvenko <kryvenkoirina93@gmail.com>
2024-05-01 19:29:39 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const backgrcolor = document.querySelector('body');
 
+let intervalId = null;
+
+function getRandomHexColor() {
+   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
+}
+startBtn.addEventListener('click', onStartBtn);
+stopBtn.addEventListener('click', onStopBtn);
+
+function changeColor() {
+    backgrcolor.style.backgroundColor = getRandomHexColor(); 
+}
+
+function onStartBtn() {
+    intervalId = setInterval(() => { changeColor(); }, 1000);
+    stopBtn.disabled = false;
+    startBtn.disabled = true;
+}
+
+function onStopBtn() {
+    clearInterval(intervalId);
+    stopBtn.disabled = true;
+    startBtn.disabled = false;
+}
